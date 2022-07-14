b7ef8ddf0716aab7897689b4ea733bbaf8068b02
thaigalhaes <thainamaga@hotmail.com>
2022-07-14 13:41:06 -0300

    jogadas

--- jogo-velha.js
+++ jogo-velha.js
@@ -1,11 +1,20 @@
+const quadrados = document.querySelectorAll(".quadrado-jogo");
+console.log(quadrados);
+
+
 const X = "X";
 const O = "O";
 
+let jogador1 = X;
+let tabela = [];
+
+tabuleiro
+
 function reiniciarJogo() {
-    
+
 }
 
 function selecionarArea(posicaoLinha, posicaoColuna) {
-    console.log ("chamando a funçao")
-    console.log (posicaoLinha, posicaoColuna);
+    console.log("chamando a funçao")
+    console.log(posicaoLinha, posicaoColuna);
 }
